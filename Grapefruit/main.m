d273ac88f337e268f60bff4ea67fc905b2636f74
Logan Shire <logan.shire@gmail.com>
2014-09-15 16:13:23 -0400

    Initial Commit

---
+++ Grapefruit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Grapefruit
+//
+//  Created by Logan Shire on 9/15/14.
+//  Copyright (c) 2014 Logan Shire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ GrapefruitTests/GrapefruitTests.m
@@ -0,0 +1,34 @@
+//
+//  GrapefruitTests.m
+//  GrapefruitTests
+//
+//  Created by Logan Shire on 9/15/14.
+//  Copyright (c) 2014 Logan Shire. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GrapefruitTests : XCTestCase
+
+@end
+
+@implementation GrapefruitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
